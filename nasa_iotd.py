0a6c310396a77ce0740376fe154e9891d6201fa2
Brian Derr <bderrly@gmail.com>
2020-12-22 22:58:31 -0800

    Add reflowText function.
    This function will add newlines if necessary in order to wrap the text
    to stay within the width of the image.
    
    I also refactored a few things and added some more command-line arguments.

--- nasa_iotd.py
+++ nasa_iotd.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
-# 2016-12-08
 
-# Grab NASA's 'Image of the Day', resize it to fit the desktop resolution,
-# add the description from the RSS feed if it is available.
-#
-# https://www.nasa.gov/multimedia/imagegallery/iotd.html
-#
-# Dependencies:
-#   feedparser
-#   pillow
-#   requests
-#   Xlib
+"""NASA Image of the Day fetcher
+
+Grab NASA's 'Image of the Day' from RSS, resize it (if necessary, keeping the
+scaling ratio the same), and add a text overlay of the description from the RSS
+feed if it is available.
+
+https://www.nasa.gov/multimedia/imagegallery/iotd.html
+
+Dependencies:
+  feedparser
+  pillow
+  requests
+  Xlib
+"""
 
 import argparse
 import os
@@ -19,21 +22,20 @@ from io import BytesIO
 
 import feedparser
 import requests
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
+from PIL import Image, ImageDraw, ImageFont
 from Xlib import display
+import Xlib.error
 
 
 MAX_FILE_SIZE = 1 << 25  # 2^25 == 33.6 MiB
 
 
 def getRssItems(count):
-    """Returns the image URI, size, and description of the latest _count_ items.
+    """Returns the image URI, size, and description of the latest count items.
     
     Returns:
-        A list of dicts containing the URI and size of the image to download and a string description of the image.
+        A list of dicts containing the URI and size of the image to download,
+        and a string description of the image.
     """
     nasa_rss = feedparser.parse('https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss')
 
@@ -46,13 +48,11 @@ def getRssItems(count):
             # If there are no enclosures then there are no images so let's move on.
             continue
 
+        # Only attempt to process the first enclosure.
         enclosure = entry.enclosures[0]
         if enclosure['type'] != 'image/jpeg':
             # If the enclosure does not have a JPEG then there's nothing to do.
             continue
-
-        if len(entry.enclosures) > 1:
-            print("Found more than one image in item {}; taking the first image.".format(entry['guid']), file=sys.stderr)
 
         image_url = enclosure['href']
         image_size = enclosure['length']
@@ -74,78 +74,133 @@ def getImage(image_url):
     return req.content
 
 
-def resizeImage(image, desktop_width, desktop_height):
-    """Resizes the image and maintains aspect ratio.
-    
-    The desktop_width and desktop_height are used to calculate the aspect ratio
-    of the original image. The ratio is then maintained when resizing the image
-    such that the maximum size is no more than screen resolution.
-    
-    Args:
-        image: A pillow Image.
-        desktop_width: An integer, the width of monitor in pixels.
-        desktop_height: An integer, the height of monitor in pixels.
-        
-    Returns:
-        A pillow Image that has been resized.
-    """
-    if image.width <= desktop_width and image.height <= desktop_height:
-        # The image is already smaller than the desktop resolution so there is
-        # no reason to resize it.
-        return image
-
-    x_ratio = float(desktop_width/image.width)
-    y_ratio = float(desktop_height/image.height)
-
-    if x_ratio < y_ratio:
-        w, h = desktop_width, int(image.height*x_ratio)
-    else:
-        w, h = int(image.width*y_ratio), desktop_height
-    return image.resize((w, h))
-
-
 def getScreenResolution():
-    """Return the desktop screen resolution using Xlib.
+    """Return the root screen resolution using Xlib.
 
     Returns:
-        An integer, width of display in pixels
-        An integer, height of the display in pixels
+        A two-tuple, the width and height of the root screen in pixels.
     """
-    d = display.Display()
-    scr = d.screen()
-    return (scr.width_in_pixels, scr.height_in_pixels)
-    
+    try:
+        d = display.Display()
+    except Xlib.error.DisplayError:
+        print("Failed attempting to retrieve display information from X. Perhaps you are not running in X?",
+                file=sys.stderr)
+        print("If you are not running in X you must supply the --resolution flag.", file=sys.stderr)
+        sys.exit(1)
+    root_screen = d.screen()
+    return (root_screen.width_in_pixels, root_screen.height_in_pixels)
+
 
 def writeImageToDisk(file_name, image):
     try:
-        image.save(file_name, 'JPEG')
-    except IOError as ioe:
-        print('IO failure: {}'.format(ioe), file=sys.stderr)
+        image.save(file_name, 'JPEG', quality=95)
+    except OSError as ose:
+        print(f"I/O failure: {ose}", file=sys.stderr)
+        sys.exit(1)
+
+
+def reflowText(text, width, font=None):
+    """Adds line breaks, if necessary, so all text can fit within width.
+
+    Args:
+        text: A string, the text to reformat.
+        width: An int, the width of the screen in pixels.
+        font: (optional) An ImageFont, the font to use on the image.
+
+    Returns:
+        A string, the reformatted text.
+    """
+    if font is None:
+        font = ImageFont.truetype('/usr/share/fonts/TTF/LiberationSerif-Regular.ttf', 18)
+    if font.getlength(text) <= width - 20:
+        return text
+
+    lines = []
+    line = []
+    for word in text.split():
+        line.append(word)
+        if font.getlength(" ".join(line)) > width - 20:
+            line.pop()
+            lines.append(line.copy())
+            line.clear()
+            line.append(word)
+    # Don't forget to append the final line!
+    lines.append(line)
+    return "\n".join([
+        " ".join(line) for line in lines
+        ])
+
+
+def renderDescription(text, image, font, font_size=24):
+    """Render the background box and text overlay.
+
+    Args:
+        text: A string, the description to render.
+        image: A PIL.Image, the image to draw on.
+        font: A PIL.ImageFont, the font to use when rendering.
+        font_size: An int, the font size for rendering the text.
+    """
+    draw = ImageDraw.Draw(image)
+    desc_font = ImageFont.truetype(font=font, size=font_size)
+    description = reflowText(text, image.width, desc_font)
+    
+    # Find the bounding box (bbox) of the rendered text. These points
+    # will be used for placing the background rectangle and text
+    # overlay. Return is a 4-tuple: (x anchor, y anchor, x destination,
+    # y destination).
+    bbox = draw.textbbox((0,0), description, font=desc_font)
+
+    # Add some extra pixels to the width and height of the bbox. This
+    # will allow for better framing of the background box and text
+    # within it.
+    bbox_padding = 20
+    textbox = (bbox[0], bbox[1], bbox[2] + bbox_padding, bbox[3] + bbox_padding)
+
+    # The four points for the background rectangle behind the text.
+    bbox_x_anchor = (image.width - textbox[2]) // 2
+    bbox_y_anchor = image.height - textbox[3] - (bbox_padding // 2) 
+    bbox_x_length = bbox_x_anchor + textbox[2]
+    bbox_y_length = bbox_y_anchor + textbox[3]
+
+    # Anchor the text a little inside the black rectangle behind it.
+    text_x_anchor = bbox_x_anchor + 5
+    text_y_anchor = bbox_y_anchor + 5
+
+    draw.rectangle((bbox_x_anchor, bbox_y_anchor, bbox_x_length, bbox_y_length), fill='black')
+    draw.text((text_x_anchor, text_y_anchor), text, fill=(20, 148, 20), font=desc_font)
 
 
 def main(argv):
-    desktop_width, desktop_height = getScreenResolution()
-
-    if desktop_width == 0 or desktop_height == 0:
-        print('Failed to get screen resolution: have {}x{}'.format(desktop_width, desktop_height), file=sys.stderr)
-        sys.exit(1)
-
     parser = argparse.ArgumentParser(
-            description='Downloads and formats the Image of the Day from nasa.org.')
+            description='Downloads, modifies, and saves the "Image of the Day" from nasa.gov.')
     parser.add_argument('-c', '--count', type=int, default=1, help="The number of RSS items to retrieve. (default: 1)")
-    parser.add_argument('-i', '--input_file', help="The name of the file to read from disk. (Does not read from RSS and adds no description to the final image.)")
-    parser.add_argument('-o', '--output_file', help="The name of the file to write to disk.")
     parser.add_argument('-d', '--directory', default=os.environ['PWD'], help='Directory to write image file. (default: $PWD)')
+    parser.add_argument('-i', '--input-file',
+            help="The name of the file to read from disk. (Does not read from RSS and adds no description to the final image.)")
+    parser.add_argument('-f', '--font', default="fonts/Play-Bold.ttf",
+            help="The path to the TrueType font to use for the image description.")
+    parser.add_argument('-o', '--output-file', help="The name of the file to write to disk.")
+    parser.add_argument('-s', '--font-size', type=int, default=24,
+            help="The size of the font when rendering the image description.")
+    parser.add_argument('-r', '--resolution', nargs=2, metavar=("WIDTH", "HEIGHT"),
+            help="The resolution of the final image. If not supplied the program will attempt to determine the resolution of the monitor using Xlib.")
+    parser.add_argument('-v', '--verbose', action="store_true", help="Print information about what the program is doing.")
     args = parser.parse_args()
 
-    # If we are operating on a local file it is probably for testing purposes
-    # because all we're going to do is resize the image and write it to disk.
+    if args.resolution is None:
+        resolution = getScreenResolution()
+    else:
+        resolution = tuple(args.resolution)
+
+    output_file = args.output_file
+
     if args.input_file is not None:
+        # If we are operating on a local file it is probably for testing purposes.
+        # All we are going to do is resize the image and write it to disk.
         with open(args.input_file, 'rb') as f:
             image_data = f.read(MAX_FILE_SIZE)
             image = Image.open(BytesIO(image_data))
-            image = resizeImage(image, desktop_width, desktop_height)
-        output_file = args.output_file
+            image.thumbnail(resolution)
         if output_file is None:
             output_file = os.path.join(args.directory, os.path.basename(args.input_file))
         writeImageToDisk(output_file, image)
@@ -153,41 +208,39 @@ def main(argv):
 
     rss_items = getRssItems(args.count)
     for item in rss_items:
-        print("Processing {}…".format(item['url']))
         image_data = getImage(item['url'])
+        if (args.verbose):
+            print(f"Processing {item['url']}…")
+
         if int(item['size']) != int(len(image_data)):
-            print("File size differs between RSS and downloaded image: {} versus {} bytes.".format(item['size'], len(image_data)))
+            print(f"File size differs between RSS and downloaded image: {item['size']} versus {len(image_data)} bytes.")
         # Cut out only the file name portion of the image's URL. This will
         # potentially be used for the output file name.
         nasa_image_filename = item['url'].rsplit('/', 1)[1]
         nasa_image = Image.open(BytesIO(image_data))
-        nasa_image = resizeImage(nasa_image, desktop_width, desktop_height)
+
+        # Resize the image (keeping aspect ratio) to fit within the specified resolution.
+        nasa_image.thumbnail(resolution)
+
+        # Create a black image the size of the desktop resolution.
+        black_image = Image.new('RGB', resolution)
+
+        # Define the origin coordinates to begin pasting the NASA image
+        # over the blank image. This is a tuple of (x, y).
+        nasa_origin= (
+                int((.5 * black_image.width) - (.5 * nasa_image.width)),
+                int((.5 * black_image.height) - (.5 * nasa_image.height)))
+
+        # Paste the NASA image over the newly created black image.
+        black_image.paste(nasa_image, nasa_origin)
 
         # Overlay the description from the RSS item over the NASA image.
         description = item.get('desc')
         if description is not None:
-            draw = ImageDraw.Draw(nasa_image)
-            font = ImageFont.truetype('/usr/share/fonts/TTF/LiberationSerif-Regular.ttf', 18)
-            w, h = font.getsize(description)
-            if int(w) > int(nasa_image.width):
-                print("The text overlay is wider than the image: {} versus {}".format(w, nasa_image.width))
-            x, y = (5, nasa_image.height-h)
-            draw.rectangle((x, y, x+w, y+h), fill='black')
-            draw.text((x, y), description, fill=(164, 244, 66), font=font)
-
-        # Create a new, blank image the size of the desktop resolution.
-        image = Image.new('RGB', (desktop_width, desktop_height))
-
-        # Define the origin coordinates to begin pasting the NASA image
-        # over the blank image. This is a tuple of (x, y).
-        box = (int(.5*desktop_width-.5*nasa_image.width),
-               int(.5*desktop_height-.5*nasa_image.height))
-
-        # Paste the NASA image over the newly created black image.
-        image.paste(nasa_image, box)
+            renderDescription(description, black_image, args.font, args.font_size)
 
         output_file = os.path.join(args.directory, nasa_image_filename)
-        writeImageToDisk(output_file, image)
+        writeImageToDisk(output_file, black_image)
 
 
 if __name__ == '__main__':
